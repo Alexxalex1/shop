49a06c5191dbd7aa9b3dbd9501d382fefbcd2b7a
Alex <alxsar98@gmail.com>
2018-12-10 17:36:55 +0300

    Comment

---
+++ resources/views/layouts/popularproducts.blade.php
@@ -0,0 +1,77 @@
+<div class="container">
+    <a href="#">
+
+        <div class="Button1">
+            Популярные товары
+
+
+        </div>
+    </a>
+</div>
+</div>
+</div>
+
+<!-- Products-->
+
+<div class="products">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3"><div class="product-img">
+                    <img src="img/Rectangle 6.2.png">
+                </div>
+                <div class="product-ttl">KitKat Mocha 165 гр</div>
+                <br>
+                <br>
+                <div class="price">
+                    269 рублей
+                    <img src="img/Корзина.png">
+                </div>
+                <div class="presence">
+                    В наличии
+
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3"><div class="product-img">
+                    <img src="img/Rectangle 6.6.png">
+                </div>
+                <div class="product-ttl">Milka Oreo 300 гр</div>
+                <br>
+                <br>
+                <div class="price">
+                    430 рублей
+                    <img src="img/Корзина.png">
+                </div>
+                <div class="presence">
+                    В наличии
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3"><div class="product-img">
+                    <img src="img/Rectangle 6.4.png">
+                </div>
+                <div class="product-ttl">Kinder Mix 237 гр</div>
+                <br>
+                <br>
+                <div class="price">
+                    1 099 рублей
+                    <img src="img/Корзина.png">
+                </div>
+                <div class="presence">
+                    В наличии
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3"><div class="product-img">
+                    <img src="img/Rectangle 6.7.png">
+                </div>
+                <div class="product-ttl">Елочная игрушка Санта с Елочкой 5*4*10 см, подвеска </div>
+                <br>
+                <div class="price">
+                    1 099 рублей
+                    <img src="img/Корзина.png">
+                </div>
+                <div class="presence">
+                    В наличии
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
