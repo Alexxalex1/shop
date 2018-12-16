5da229419d5683198ce7dd32d2584f89fcae4b9c
Alex <alxsar98@gmail.com>
2018-12-17 01:26:06 +0300

    17_12_18_01.30

--- resources/views/inputfeedback.blade.php
+++ resources/views/inputfeedback.blade.php
@@ -9,11 +9,11 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{('css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Material Design Bootstrap -->
-    <link href="{{asset('css/mdb.min.css')}}" rel="stylesheet">
+    <link href="{{('css/mdb.min.css')}}" rel="stylesheet">
     <!-- Your custom styles (optional) -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{('css/style.css')}}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="InputFeedback.css">
 
 </head>
