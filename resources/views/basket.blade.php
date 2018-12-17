b9c060bd2d308a140dda9b7c7a8ff8b1f8a26d6d
Alex <alxsar98@gmail.com>
2018-12-17 14:18:15 +0300

    17_12_18_01.30

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -44,7 +44,7 @@
 
 
 
-   {{-- $request->session()->push('basket.values', $request->id);--}}
+    {{-- $request->session()->push('basket.values', $request->id);--}}
     {{-- $items, $currentItem --}}
 
     {{--@foreach ($items as $item)--}}
@@ -93,15 +93,13 @@
 
             <div class="col-xl-2">
                 <div class="price">
-                    <div class="counter-elem__arrow counter-elem__plus">+</div>
-                    <input type="text" class="counter-elem__input splash-input" value="1">
-                    <div class="counter-elem__arrow counter-elem__minus">-</div>
+                   1
                 </div>
             </div>
 
             <div class="col-xl-2">
                 <div class="price">
-                    {{$item->price*2}} ₽
+                    {{$item->price}} ₽
                 </div>
             </div>
 
@@ -125,41 +123,8 @@
 
 
 </div>
-        <style>
-            div{
-                cursor: pointer;
-            }
-            .counter-elem__arrow{
-                margin-left: 50%;
-                font-size: 35px;
-                margin-top: 10px;
-            }
-            input{
-                width: 10%;
-                margin-left: 50%;
-                margin-top: 10px;
-            }
 
-        </style>
-
-        <script>
-            var splashInput = document.querySelector(".splash-input");
-            var splashVal = parseFloat(splashInput.value);
-            document.querySelector(".counter-elem__plus").onclick = function() {
-                splashInput.value = ++splashVal;
-            }
-            document.querySelector(".counter-elem__minus").onclick = function() {
-                if (splashVal > 1) {
-                    splashInput.value = --splashVal;
-                }
-            }
-        </script>
-
-<footer class="page-footer font-small yellow">
-
-    <!-- Copyright -->
-    <div class="footer-copyright text-center py-3"> Sincerely, NYG team!
-        <a href="https://mdbootstrap.com/education/bootstrap/"> </a>
+@include('layouts.footer')
     </div>
     <!-- JQuery -->
     <script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js')}}"></script>
@@ -169,6 +134,6 @@
     <script type="text/javascript" src="{{ asset('js/bootstrap.min.js')}}"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="{{ asset('js/mdb.min.js')}}"></script>
-</footer>
+
 </body>
 </html>
--- resources/views/sweets.blade.php
+++ resources/views/sweets.blade.php
@@ -25,74 +25,35 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-xl-2">
-            <div class="NameCurrentPage">
-                Сладости
-            </div>
-
-
-            <div class="areaDropButton">
-                <button id="shower" ">Все товары</button>
-                <div class="allProducts" style="display: none;">
-                    <div id="showerDown">
-                        Сладости
-                    </div>
-                    <div id="showerDown">
-                        Конфеты
-                    </div>
-                    <div id="showerDown">
-                        Наборы конфеты
-                    </div>
-                </div>
-            </div>
-
-
-            <script>
-                $("#shower").click(function () {
-                    $(".allProducts").slideDown(500);
-                });
-            </script>
-
         </div>
-        <div class="col-xl-10">
+        <div class="col-xl-12">
             <!-- Products-->
             <div class="products">
                 <div class="row">
                     @foreach ($items as $item)
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-4"><div class="product-img">
-                            <img src="{{ asset('img/'.$item->image) }}" width="100%" height="100%">
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3"><div class="product-img">
+                                <img src="{{ asset('img/'.$item->image) }}" width="100%" height="100%">
+                            </div>
+                            <div class="product-ttl">{{ $item->name}} </div>
+                            <br>
+                            <br>
+                            <div class="price">
+                                {{ $item->price }} рублей
+                                <a href="{{ route('basket.add', ['id' => $item->id] )}}"><img src="img/Корзина.png"></a>
+                            </div>
+                            <div class="presence">
+                                В наличии
+                            </div>
                         </div>
-                        <div class="product-ttl">{{ $item->name}} </div>
-                        <br>
-                        <br>
-                        <div class="price">
-                            {{ $item->price }} рублей
-                            <a href="{{ route('basket.add', ['id' => $item->id] )}}"><img src="img/Корзина.png"></a>
-                        </div>
-                        <div class="presence">
-                            В наличии
-                        </div>
-                    </div>
                     @endforeach
                 </div>
             </div>
-
         </div>
     </div>
 </div>
-
 <!-- Footer-->
-
-<footer class="page-footer font-small yellow">
-
+@include('layouts.footer')
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3"> Sincerely, NYG team!
-        <a href="https://mdbootstrap.com/education/bootstrap/"> </a>
-    </div>
-    <!-- Copyright -->
-
-</footer>
-
 
 <script type="text/javascript" src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
 <!-- Bootstrap tooltips -->
@@ -102,6 +63,5 @@
 <!-- MDB core JavaScript -->
 <script type="text/javascript" src="{{asset('js/mdb.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('jquery/jquery.js')}}"></script>
-
 </body>
 </html>
