a2d4f460fbff6f3597e3a88b721e45975eb948fe
Alex <alxsar98@gmail.com>
2018-12-17 17:54:28 +0300

    Напоследок

--- resources/views/nextbasket.blade.php
+++ resources/views/nextbasket.blade.php
@@ -8,58 +8,15 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Material Design Bootstrap -->
-    <link href="css/mdb.min.css" rel="stylesheet">
+    <link href="{{asset('css/mdb.min.css')}}" rel="stylesheet">
     <!-- Your custom styles (optional) -->
-    <link rel="stylesheet" type="text/css" href="basket2.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('basket2.css')}}">
 
 </head>
 <body>
-<nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
-
-    <div class="container">
-
-        <a href="index.html" class="navbar-brand  waves-effect">
-            <img src="img/Logo.png" width="60" height="56" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <div class="col-xl-11">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="products.html" class="nav-link waves-effect"><strong>Сладости</strong> </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link waves-effect">
-                            <strong>Украшения</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link waves-effect">
-                            <strong>Наборы</strong></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="feedback.html" class="nav-link waves-effect ">
-                            <strong> Отзывы</strong></a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xl-1">
-                <ul class="navbar-nav">
-
-                    <li class="nav-item">
-                        <a href="basket1.html" class="nav-link waves-effect"> <img src="img/Корзина.png"></a>
-                    </li>
-                </ul>
-
-            </div>
-
-        </div>
-    </div>
-</nav>
+@include('layouts.navbar')
 
 <div class="container">
     <div class="basket">
